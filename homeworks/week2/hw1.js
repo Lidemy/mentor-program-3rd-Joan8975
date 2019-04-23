991956cc95172c6eb966b6ef4e5f9fd0ac29bf06
Joan8975 <Joan8975@users.noreply.github.com>
2019-04-23 20:27:08 +0800

    hw1 modified

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,7 +1,7 @@
 function printStars(n) {
   const i = '*';
   if (n >= 1 && n <= 30) {
-    console.log(`${i} + '\n'`.repeat(n - 1) + i);
+    console.log(`${i}\n`.repeat(n - 1) + i);
   }
 }
 printStars(5);
