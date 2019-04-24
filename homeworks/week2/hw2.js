d50aaf34cb99bf39440615e760e7d858f496bfb7
Joan8975 <Joan8975@users.noreply.github.com>
2019-04-24 21:06:19 +0800

    hw2 done

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,7 @@
 function capitalize(str) {
-  return str;
+  const first = str.slice(0, 1);
+  const rest = str.slice(1);
+  const uppercase = first.toUpperCase();
+  return (uppercase + rest);
 }
-
 console.log(capitalize('hello'));
