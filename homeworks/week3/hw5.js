84bccbdcc558ba56b331276c9ca306cea05957f5
Joan8975 <Joan8975@users.noreply.github.com>
2019-05-02 11:03:03 +0800

    hw5 done

--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -1,5 +1,29 @@
 function add(a, b) {
-  console.log(a, b);
+  const x = a.split('').reverse();
+  const y = b.split('').reverse();
+  if (a.length < b.length) {
+    for (let i = 0; i < b.length - a.length; i += 1) {
+      x.push('0');
+    }
+  } else {
+    for (let i = 0; i < a.length - b.length; i += 1) {
+      y.push('0');
+    }
+  }
+  x.push('0');
+  y.push('0');
+  const result = [];
+  for (let i = 0; i < y.length; i += 1) {
+    if (result[i - 1] >= 10) {
+      result[i] = Number(x[i]) + Number(y[i]) + 1;
+      result[i - 1] %= 10;
+    } else {
+      result[i] = Number(x[i]) + Number(y[i]);
+    }
+  }
+  if (result[y.length - 1] === 0) {
+    result.splice(y.length - 1, 1);
+  }
+  return result.reverse().join('');
 }
-
 module.exports = add;
