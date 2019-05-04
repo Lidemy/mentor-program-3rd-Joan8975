f3783aa3e1775f9e7019d8caac11ffb11a749099
Joan8975 <Joan8975@users.noreply.github.com>
2019-05-04 12:13:16 +0800

    hw6 done

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,9 +1,11 @@
 function stars(n) {
   const result = [];
   let sum = '';
-  for (let i = 1; i < n + 1; i += 1) {
-    sum += '*';
-    result.push(sum);
+  if (n > 0 && n < 30) {
+    for (let i = 1; i < n + 1; i += 1) {
+      sum += '*';
+      result.push(sum);
+    }
   }
   return result;
 }
